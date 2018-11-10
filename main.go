2ada3da0faefb2efc53afaa0119edc0fce0b47df
fr-xiaoli <xiaoli.shen@fastretailing.com>
2018-11-10 11:58:40 +0900

    pointer exec

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	b := a
+	c := &a
+
+	fmt.Printf("====== Values ====== \n a = %v\n b = %v\n c = %v\n *c = %v\n",
+		a, b, c, *c)
+	fmt.Printf("====== Types ======= \n a: %T\n b: %T\n c: %T\n *c: %T\n",
+		a, b, c, *c)
+
+	b = 43
+	fmt.Printf("b = 43, now a = %v\n", a)
+	*c = 44
+	fmt.Printf("*c = 44, now a = %v, b = %v, c = %v\n", a, b, c)
+	fmt.Println("==============")
+	d := c
+	*d = 45
+	fmt.Printf("d := c, *d = 45\n now a = %v, b = %v\n c = %v, *c = %v, d = %v\n",
+		a, b, c, *c, d)
+}
